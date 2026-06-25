1e3d7b9fc41407f750e530464afe246fe52f1b34
MoneySMP-alt <altt2t2345734@gmail.com>
2026-06-26 01:38:37 +0300

    Create data.js with player information
    Added initial player data with one player entry.

---
+++ data.js
@@ -0,0 +1,9 @@
+// ─────────────────────────────────────────
+//  ADD YOUR PLAYERS HERE
+//  team options: 'yellow' | 'blue' | 'green' | 'red'
+// ─────────────────────────────────────────
+
+const PLAYERS = [
+  // { name: 'PlayerName', money: 50000, team: 'red' },
+  { name: 'D4ker', money: 1000000, team: 'green' },
+];
